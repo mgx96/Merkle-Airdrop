7fa9e98996d85e16c4e639b814fe5f6296220028
Malek Sharabi <maleksharabi@Mac.lan>
2026-05-07 14:15:49 +0200

    merkle airdrop contract created and claim verification finalized

--- src/MerkleAirdrop.sol
+++ src/MerkleAirdrop.sol
@@ -2,4 +2,32 @@
 
 pragma solidity ^0.8.30;
 
-contract MerkleAirdrop {}
+import {IERC20, SafeERC20} from "../lib/openzeppelin-contracts/contracts/token/ERC20/utils/SafeERC20.sol";
+import {MerkleProof} from "../lib/openzeppelin-contracts/contracts/utils/cryptography/MerkleProof.sol";
+
+contract MerkleAirdrop {
+    using SafeERC20 for IERC20;
+
+    error MerkleAirdrop__InvalidProof();
+
+    address[] claimers;
+    bytes32 private immutable i_merkleRoot;
+    IERC20 private immutable i_airdropToken;
+
+    event Claimed(address account, uint256 amount);
+
+    constructor(bytes32 merkleRoot, IERC20 airdropToken) {
+        i_merkleRoot = merkleRoot;
+        i_airdropToken = airdropToken;
+    }
+
+    function claim(address account, uint256 amount, bytes32[] calldata merkleProof) external {
+        //we concat and double hash to prevent a second preimage attack, where an attacker can find another input that produces the same hash as the leaf node.
+        bytes32 leaf = keccak256(bytes.concat(keccak256(abi.encode(account, amount))));
+        if (!MerkleProof.verify(merkleProof, i_merkleRoot, leaf)) {
+            revert MerkleAirdrop__InvalidProof();
+        }
+        emit Claimed(account, amount);
+        i_airdropToken.safeTransfer(account, amount);
+    }
+}
