6a3f9e68f7ce7aaac0ee31a7703920223a7ddca1
Malek Sharabi <maleksharabi@Mac.lan>
2026-05-11 20:28:33 +0200

    fixed format

--- script/DeployMerkleAirdrop.s.sol
+++ script/DeployMerkleAirdrop.s.sol
@@ -7,7 +7,6 @@ import {PrdxToken} from "../src/PrdxToken.sol";
 import {IERC20} from "../lib/openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
 
 contract DeployMerkleAirdrop is Script {
-
     bytes32 private s_merkleRoot = 0xaa5d581231e596618465a56aa0f5870ba6e20785fe436d5bfb82b08662ccc7c4;
     uint256 private s_amountToTransfer = 4 * 25 * 1e18;
 
@@ -18,11 +17,10 @@ contract DeployMerkleAirdrop is Script {
         token.mint(token.owner(), s_amountToTransfer);
         token.transfer(address(airdrop), s_amountToTransfer);
         vm.stopBroadcast();
-        return(airdrop, token);
+        return (airdrop, token);
     }
 
-    function run() external returns (MerkleAirdrop, PrdxToken)  {
+    function run() external returns (MerkleAirdrop, PrdxToken) {
         return deployMerkleAirdrop();
     }
-
 }
