a007d993362eca58a87a34d3a405083e7f83229e
Malek Sharabi <maleksharabi@Mac.lan>
2026-05-19 16:21:48 +0200

    split ECDSA signature into its components

--- script/Interact.s.sol
+++ script/Interact.s.sol
@@ -7,16 +7,33 @@ import {DevOpsTools} from "../lib/foundry-devops/src/DevOpsTools.sol";
 import {MerkleAirdrop} from "../src/MerkleAirdrop.sol";
 
 contract ClaimAirdrop is Script {
+    error ClaimAirdrop__InvalidSignatureLength();
+    //using the default anvil address
+    address CLAIMING_ADDRESS = 0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266;
+    uint256 CLAIMING_AMOUNT = 25 * 1e18;
+    bytes32 PROOF_ONE = 0xd1445c931158119b00449ffcac3c947d028c0c359c34a6646d95962b3b55c6ad;
+    bytes32 PROOF_TWO = 0xe5ebd1e1b5a5478a944ecab36a9a954ac3b6b8216875f6524caa7a1d87096576;
+    bytes32[] merkleProof = [PROOF_ONE, PROOF_TWO];
+    // derived from "cast wallet sign --no-hash" running on anvil node
+    bytes private SIGNATURE =
+        hex"e6daba7e95f9099d91a9302ad015d956e1798978b0632a0eb8798d5e13f7f734407ba27caaded62631223d997180924ead95c3d5fe8e7953ba8658b91e115e051b";
+
     function claimAirdrop(address airdrop) public {
-        //using the default anvil address
-        address CLAIMING_ADDRESS = 0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266;
-        uint256 CLAIMING_AMOUNT = 25 * 1e18;
-        bytes32 PROOF_ONE = 0xd1445c931158119b00449ffcac3c947d028c0c359c34a6646d95962b3b55c6ad;
-        bytes32 PROOF_TWO = 0xe5ebd1e1b5a5478a944ecab36a9a954ac3b6b8216875f6524caa7a1d87096576;
-        bytes32[] merkleProof = [PROOF_ONE, PROOF_TWO];
         vm.startBroadcast();
-        MerkleAirdrop.claim(CLAIMING_ADDRESS, CLAIMING_AMOUNT, merkleProof, v, r, s);
+        (uint8 v, bytes32 r, bytes32 s) = splitSignature(SIGNATURE);
+        MerkleAirdrop(airdrop).claim(CLAIMING_ADDRESS, CLAIMING_AMOUNT, merkleProof, v, r, s);
         vm.stopBroadcast();
+    }
+
+    function splitSignature(bytes memory signature) public pure returns (uint8 v, bytes32 r, bytes32 s) {
+        if (signature.length != 65) {
+            revert ClaimAirdrop__InvalidSignatureLength();
+        }
+        assembly {
+            r := mload(add(signature, 32))
+            s := mload(add(signature, 64))
+            v := byte(0, mload(add(signature, 96)))
+        }
     }
 
     function run() external {
